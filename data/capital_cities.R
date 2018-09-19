970af2df35b459e3a5cde36d0d42705d2fbbac60
Steff <stephanie.kobakian@gmail.com>
2018-09-19 15:14:20 +1000

    Capital cities data set added

---
+++ data/capital_cities.R
@@ -0,0 +1,13 @@
+#' Location of capital cities of Australia
+#'
+#' A table containing the names, latitudes and longitudes of Australian capital
+#' cities
+#'
+#' @format A data frame with 8 rows and 3 variables:
+#' \describe{
+#'   \item{city}{name of city}
+#'   \item{longitude}{longitude of city}
+#'   \item{latitude}{latitude of city}
+#' }
+#'
+capital_cities
