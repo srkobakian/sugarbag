fc26b1a2faddd70f567cd0551aced314f2db0b0a
Steff <stephanie.kobakian@gmail.com>
2018-09-20 14:40:08 +1000

    buffer inclusion

--- R/create_hexmap.R
+++ R/create_hexmap.R
@@ -1,15 +1,16 @@
 #' create_hexmap
+#'
 #' Allocates each polygon in a shape file to a grid point to create a map of
 #' tesselated hexagons. The spatial relationships of areas are preserved while
 #' the geographic chape of each area is lost.
 #'
-#' @param shp a file containing polygons to allocate to a hexmap
-#' @param bbox table with columns minimum and maximum and rows
-#' containing longitude and latitude values
+#'
+#' @param shp_path character vector location of shape file, extension .shp
+#' @param buffer a float distance in degrees to exapnd hexagon grid
 #' @param radius a float value in degrees for the diameter of the hexagons
-#' @param focal_points
-#' @param expand_grida a float value to expand the grid beyond the minimum
-#'  and maximum longitudes and latitudes
+#' @param focal_points a data frame of reference locations when allocating
+#' hexagons, capital cities of Australia are used in the example
+#' @param export_shp export the simple features set including centroids
 #'
 #' @return
 #' @export
@@ -17,27 +18,48 @@
 #' @importFrom sf st_read
 #'
 #' @examples
-#' #shp <- "C:/Users/steff/Documents/atlas/hexmap/data/LGA_2011_AUST/LGA_2011_AUST.shp"
+#' \dontrun{
+#' shp <- "C:/Users/steff/Documents/atlas/hexmap/data/LGA_2011_AUST/LGA_2011_AUST.shp"
 #'
+#' create_hexmap(shp, buffer = NULL, radius = "auto", export_shp = FALSE,
+#' focal_points = system.file("data","capital_cities.Rda", package = "sugaRbag"))
+#' }
 #'
-create_hexmap <- function(shp, bbox = NULL, radius = "auto", focal_points = NULL, expand_grid = 0.1) {
+create_hexmap <- function(shp_path, buffer = NULL, radius = "auto", focal_points = NULL, export_shp = FALSE) {
 
     # Read in ESRI shape file, remove null geometries, transform projection
-    shp <- sf::st_read(shp)
+    if (class(shp_path) == "character") {
+        shp <- tryCatch(expr = sf::st_read(shp),
+            error=function(e) print("Argument for shp could not be read as sf object."))
+    }
+
     shp_polys <- shp %>%
         dplyr::filter(!sf::st_is_empty(sf::st_geometry(.))) %>%
         sf::st_transform(shp_polys, crs = 3112, '+init=epsg:3112 +proj=longlat +ellps=GRS80')
+# add message that polygons were dropped?
 
-    # find the boundary points
-    if (is.null(bbox)){
+    # find the bbox
+    if (is.null(buffer)){
         bounds <- sf::st_bbox(shp, crs = 3112)
         bbox <- tibble::tibble(min = c(bounds[1], bounds[2]),
             max = c(bounds[3], bounds[4]))
     }
+
     # if matrix, convert to tibble
     if (!("tbl" %in% class(bbox))){
         bbox <- tibble::as.tibble(bbox)
     }
+
+    # if there was no buffer distance, make it 10% of bounds
+    if (is.null(buffer)){
+        buffer <- max(c(bbox$max[1] - bbox$min[1]), (bbox$max[2] - bbox$min[2])) * 0.1
+    }
+
+    # create buffer zone for grid points
+    # takes so long!
+    full <- sf::st_combine(shp$geometry)
+    buffer_zone <- sf::st_buffer(full, buffer)
+
     # if radius is not supplied, choose radius TODO: tune this
     if (radius == "auto"){
         radius <- (bbox$max[1] - bbox$min[1])/(bbox$max[2] - bbox$min[2])/5
@@ -52,6 +74,10 @@ create_hexmap <- function(shp, bbox = NULL, radius = "auto", focal_points = NULL
 
     # Add centroids to shapes set
     shp_polys <- shp_polys %>% mutate(long_c = centroids$X, lat_c = centroids$Y)
+
+    if (export_shp) {
+        suppressWarnings(shp <<- shp_polys)
+    }
 
     if (!is.null(focal_points)) {
         # distance between centroids and all focal points
