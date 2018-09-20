525656c6894779d9ed1f9b05a7b300c97ee9c78c
Dianne Cook <visnut@gmail.com>
2018-09-20 15:18:40 +1000

    address to find shapefiles

--- inst/examples/reading_shapefile.R
+++ inst/examples/reading_shapefile.R
@@ -1,1 +1,3 @@
 # This code was used to process the raw shape files
+# Download shape file from ABS web site
+# http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.001July%202016?OpenDocument
