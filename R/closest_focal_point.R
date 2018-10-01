294c13a233d92632526289aa4a81c93a2cc7763b
Steff <stephanie.kobakian@gmail.com>
2018-10-01 22:28:51 +1000

    Checks names of columns

--- R/closest_focal_point.R
+++ R/closest_focal_point.R
@@ -3,25 +3,47 @@
 #' For one row of an sf data frame, calculate the distance to the closest focal
 #' point. Return the name of the focal point, and the angle between focal point and centroid.
 #'
-#' @param polygon
+#' @param centroid
 #' @param focal_points
 #'
 #' @return data frame containing the name and location of the closest focal
 #' @export
 #'
 #' @examples
-closest_focal_point <- function(polygon, focal_points) {
+closest_focal_point <- function(centroid, focal_points = as.matrix(focal_points)) {
+
+
+    if ("long" %in% colnames(focal_points)) {
+        colnames(focal_points)[which(colnames(focal_points) == "long")] <- "longitude"
+    }
+    if ("lat" %in% colnames(focal_points)) {
+        colnames(focal_points)[which(colnames(focal_points) == "lat")] <- "latitude"
+    }
+
+    # When applying to hexagon grid
+    if ("hex_long" %in% colnames(focal_points)) {
+        colnames(focal_points)[which(colnames(focal_points) == "hex_long")] <- "longitude"
+
+        colnames(focal_points)[which(colnames(focal_points) == "hex_lat")] <- "latitude"
+
+        fp_matrix <- as.matrix(bind_cols(longitude = focal_points$longitude, latitude = focal_points$latitude))
+    }
+
+
+    # create a martix for distance calculations
+    fp_matrix <- as.matrix(bind_cols(longitude = focal_points$longitude, latitude = focal_points$latitude))
 
     focal_distance <- geosphere::distVincentyEllipsoid(
-        c(polygon$long_c, polygon$lat_c), focal_points[ ,2:3],
+        c(centroid$longitude, centroid$latitude), fp_matrix,
         a=6378160, b=6356774.719, f=1/298.257222101)
 
-    # closest city
+    # closest point
     focal_distance_df <- focal_points %>% dplyr::bind_cols(., focal_distance = focal_distance) %>% top_n(-1, wt = focal_distance)
 
     # angle from city to centroid
-    focal_distance_df$angle <- geosphere::finalBearing(focal_distance_df[ ,2:3], c(polygon$long_c, polygon$lat_c),
+    focal_distance_df$angle <- geosphere::finalBearing(focal_distance_df[,2:3],c(centroid$longitude, centroid$latitude),
         a=6378160, f=0)
 
     return(focal_distance_df)
 }
+
