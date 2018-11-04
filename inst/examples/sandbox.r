c9ec3389da0b392476e0f2a65ca73e9919e3220e
Steff <stephanie.kobakian@gmail.com>
2018-11-04 16:32:40 +1100

    Use sandbox for VIC

--- inst/examples/sandbox.r
+++ inst/examples/sandbox.r
@@ -1,12 +1,22 @@
 # This is my test code
 library(tidyverse)
 library(purrr)
-library(sugaRbag)
+#library(sugaRbag)
 shp_path <- system.file("data","sa2_2011.Rda", package = "sugaRbag")
 load(system.file("data","capital_cities.Rda", package = "sugaRbag"))
 
 shp_sf <- read_shape(shp_path, simplify = TRUE)
-centroids <- create_centroids(shp_sf)
+
+
+###############################################################################
+# consider only VIC
+vic_sf <- shp_sf %>% filter(STE_NAME11 == "Victoria")
+
+sf_id = "SA2_NAME11"
+centroids <- create_centroids(vic_sf, id = sf_id)
+
+# centroids <- create_centroids(shp_sf)
+
 
 bbox <- tibble::tibble(min = c(min(centroids$longitude),
                                min(centroids$latitude)),
@@ -38,9 +48,9 @@ centroids <- centroids %>%
     mutate(long_int = round((longitude-min(hex_grid$hex_long))/(max(hex_grid$hex_long)-min(hex_grid$hex_long))*nlong, 0),
            lat_int = round((latitude-min(hex_grid$hex_lat))/(max(hex_grid$hex_lat)-min(hex_grid$hex_lat))*nlat, 0))
 
-ggplot(hex_grid, aes(x=hex_long_int, y=hex_lat_int)) +
-    geom_point(size=0.02) +
-    geom_point(data=centroids, aes(x=long_int, y=lat_int), colour="red", size=0.1)
+#ggplot(hex_grid, aes(x=hex_long_int, y=hex_lat_int)) +
+#    geom_point(size=0.02) +
+#    geom_point(data=centroids, aes(x=long_int, y=lat_int), colour="red", size=0.1)
 
 
 lat_size = round(nlat/20,0)
@@ -97,7 +107,7 @@ long_windows <- map(.x = nlong_list, .f = long_window, centroids, nlong)
 # find the min and max longitude for each latitude
 range_cols <- map_dfr(.x = long_windows, .f = function(x) { x %>%
         dplyr::summarise(
-            lat_min = ifelse(is_empty(lat_int), NA, min(x$lat_int)-),
+            lat_min = ifelse(is_empty(lat_int), NA, min(x$lat_int)),
             lat_max = ifelse(is_empty(lat_int), NA, max(x$lat_int))
         )}
 )
@@ -118,10 +128,56 @@ buff_grid <- hex_grid %>%
     left_join(., av_range_rows, by = c("hex_lat_int" = "lat_id")) %>%
     left_join(., av_range_cols, by = c("hex_long_int" = "long_id")) %>%
     rowwise %>%
-    mutate(long_buffer = ifelse(between(hex_long_int,mean_long_min, mean_long_max), "in", "out")) %>%
-    mutate(lat_buffer = ifelse(between(hex_lat_int,mean_lat_min, mean_lat_max), "in", "out"))
+    mutate(long_buffer = ifelse(between(hex_long_int,mean_long_min - hex_buffer,
+        mean_long_max + hex_buffer), "in", "out")) %>%
+    mutate(lat_buffer = ifelse(between(hex_lat_int,mean_lat_min - hex_buffer,
+        mean_lat_max + hex_buffer), "in", "out")) %>%
     filter(lat_buffer =="in" | long_buffer == "in")
 
-ggplot(buff_grid, aes(x=hex_long_int, y=hex_lat_int)) +
-    geom_point(size=0.02) +
-    geom_point(data=centroids, aes(x=long_int, y=lat_int), colour="red", size=1)
+
+#ggplot(buff_grid, aes(x=hex_long_int, y=hex_lat_int)) +
+#    geom_point(size=0.02) +
+#    geom_point(data=centroids, aes(x=long_int, y=lat_int), colour="red", size=1)
+
+
+# FIND CLOESEST FOCAL POINT,
+# provides columns "points", "longitude1", "latitude1", "focal_distance", "angle"
+s_centroids <- split(x = centroids, seq(NROW(centroids)))
+
+centroids <- bind_cols(centroids,
+    purrr::map_dfr(.x = s_centroids,
+        .f = closest_focal_point,
+        focal_points = capital_cities))
+
+
+###############################################################################
+# ALLOCATE POLYGON CENTROIDS TO A HEXMAP POINT
+hexmap_allocation <- allocate(centroids = centroids,
+    hex_grid = buff_grid,
+    hex_size = 0.02,
+    filter_dist = 5000,
+    focal_points = capital_cities,
+    show_progress = TRUE,
+    id = sf_id)
+
+# Join to the shape file data set
+# hexmap_allocation <- left_join(shp_sf, hexmap_allocation)
+
+# for VIC
+hexmap_df <- left_join(vic_sf, hexmap_allocation, by = c("SA2_NAME11"))
+
+# JOIN OTHER DATA HERE
+###############################################################################
+# Maps
+library(ggthemes)
+
+g_s <- ggplot(hexmap_df) + geom_sf(aes(fill=SA4_NAME11)) + guides(fill = FALSE)
+
+g_h <- ggplot(hexmap_df) + geom_hex(aes(x = hex_long, y = hex_lat, fill=SA4_NAME11, label = SA2_NAME11),position = "identity", stat = "identity") + guides(fill = FALSE)
+
+# Interctive MAP
+library(plotly)
+ggplotly(g_h)
+
+# plot to check long lat against Melbourne
+#ggplot(hex_grid, aes(hex_long, hex_lat)) + geom_point() +geom_point(data = centroid_allocation, aes(hex_long, hex_lat), colour= "red") + geom_point(aes(x=longitude, y=latitude),centroid, colour="blue") + geom_point(data = centroid_allocation %>% filter(SA2_NAME11 == "Melbourne"), aes(hex_long, hex_lat), colour= "yellow")
