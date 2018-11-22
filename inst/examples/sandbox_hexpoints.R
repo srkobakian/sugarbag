6302d49f5e5450ac2712afb42d7f5a9d124713a8
Steff <stephanie.kobakian@gmail.com>
2018-11-23 10:39:33 +1100

    Create hexagon polygons directly, 7 points per poly

--- inst/examples/sandbox_hexpoints.R
+++ inst/examples/sandbox_hexpoints.R
@@ -6,40 +6,35 @@ load(system.file("data","hexmap_qld_3.Rda", package = "sugaRbag"))
 hex_size <- 0.3
 
 # only use a subset
-data <- hexmap_qld_3[1:5,]
+data <- hexmap_qld_3
 sf_id = "SA2_NAME11"
 
-hex_points <- function(data) {
 
-points <-
-    c(0, 60, 120, 180, 240, 300, 360) %>%
-    map_dfr(~ {
-        point <- geosphere::destPoint(
-            p = c(data$hex_long, data$hex_lat),
+# Calculate 7 points, given the set of degrees and original hex long and lat
+hex_points <- function(area, degrees = c(0, 60, 120, 180, 240, 300, 360)) {
+    points <- geosphere::destPoint(
+            p = c(area$hex_long, area$hex_lat),
             d = 111139*hex_size,
-            a=6378160, f=1/298.257222101, b = .x)
+            a=6378160, f=1/298.257222101, b = degrees)
 
-        return(tibble(hexv_long = point[1],
-            hexv_lat = point[2]))
-    }
-    ) %>% mutate(data[sf_id])
+    points_tbl <- tibble(hexv_long = points[,1],
+            hexv_lat = points[,2]) %>%
+        mutate(sf_id = area[[sf_id]])
 
-return(points)
+    colnames(points_tbl)[3] <- sf_id
+
+    return(points_tbl)
 }
 
 
+hex_points_df <- data %>%
+    split(.[[sf_id]]) %>%
+    map_dfr(hex_points)
 
-points <- data %>% rowwise %>%
-    map_dfr(~ {
-        browser()
-        hpoints <- hex_points(data = .x)
-    return(hpoints)})
-
-
+system.time(
 hex_points_df <- full_join(data,
-    points,
-    by = c(SA2_NAME11 = "sf_id"))
-
+    hex_points_df,
+    by = c("SA2_NAME11")))
 
 
 # create a data frame with 7 points per hexagon centroids
@@ -47,6 +42,6 @@ hex_points_df <- full_join(data,
 # find how many rows in the tibble
 
 
-ghex <- ggplot(hex_points_df, aes(hex_point_long, hex_point_lat)) +
-    geom_point(aes(label = SA2_NAME11)) + geom_point(aes(hex_long, hex_lat), colour = "red")
-ggplotly(ghex)
+ghex <- ggplot(hex_points_df, aes(hexv_long, hexv_lat)) +
+    geom_polygon(aes(label = SA2_NAME11, group = SA2_NAME11))
+ghex
