892d988b2c2b08cb78b5750f0b4d2d22beffef48
Steff <stephanie.kobakian@gmail.com>
2018-11-23 17:44:31 +1100

    begin replicating hex points to line up with polys

--- inst/examples/sandbox_hexpoints.R
+++ inst/examples/sandbox_hexpoints.R
@@ -46,3 +46,49 @@ hex_points_df <- full_join(data,
 ghex <- ggplot(hex_points_df, aes(hexv_long, hexv_lat)) +
     geom_polygon(aes(label = SA2_NAME11, group = SA2_NAME11), fill=NA, colour = "black") + coord_equal()
 ghex
+
+x = 1
+
+# joining hex and geo points
+hex_points_df <- fortified_qld_3 %>% group_by(SA2_NAME11) %>% summarise(count = n()) %>% left_join(hex_points_df)
+
+s_hpdf <- split(hex_points_df, hex_points_df[[sf_id]])
+quotient <- s_hpdf[[x]]$count[1] %/% 6 # the quotient, amount for points 1-6
+modulous <- s_hpdf[[x]]$count[1] %% 6 # the remainder, amount to go into 7th point
+
+# situation1: 7 geo points, match 7 hex points with 7 geo
+if (quotient == 1 && modulous = 0) {
+    s_hpdf[[x]] %>% dplyr::select(hexv_long, hexv_lat, hexv_id)
+}
+
+
+
+# situation2: < 7 geo points, match 7 hex points with < 7 geo by repeating geo points
+
+if (quotient == 0) {
+    # repeat polygon points to make 6 or 7 hex points
+    # for example robertson has 5 points
+    s_hpdf[["Robertson"]] %>%
+        dplyr::select(hexv_long, hexv_lat, hexv_id)
+
+}
+
+
+# situation3: > 7 geo points, match 7 hex points with > 7 geo by repeating hex points
+quotient <- s_hpdf[["Brisbane City"]]$count[1] %/% 6
+modulous <- s_hpdf[["Brisbane City"]]$count[1] %% 6
+if (quotient > 0) {
+    hp <- s_hpdf[["Brisbane City"]] %>%
+        dplyr::select(hexv_long, hexv_lat, hexv_id)
+
+    hp1_7 <- bind_rows(
+        bind_cols(hexv_long = rep(hp$hexv_long[1:6], times = quotient),
+    hexv_lat = rep(hp$hexv_lat[1:6], times = quotient),
+    hexv_id = rep(hp$hexv_id[1:6], times = quotient)) %>% arrange(hexv_id),
+
+        bind_cols(hexv_long = rep(hp$hexv_long[7], times = modulous),
+        hexv_lat = rep(hp$hexv_lat[7], times = modulous),
+        hexv_id = rep(hp$hexv_id[7], times = modulous)))
+
+
+}
