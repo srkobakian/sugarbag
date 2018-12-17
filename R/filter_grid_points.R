01d26b2295448b6cd5855e4e0d61f95cb9379f29
Steff <stephanie.kobakian@gmail.com>
2018-12-17 11:52:09 +1100

    Include hexagon size in grid filtering

--- R/filter_grid_points.R
+++ R/filter_grid_points.R
@@ -22,18 +22,38 @@
 #'
 #'
 #'
-filter_grid_points <- function(f_grid, f_centroid, focal_points = NULL, f_dist = filter_dist, angle_width = width){
+filter_grid_points <- function(f_grid, f_centroid, focal_points = NULL, f_dist = filter_dist, angle_width = width, h_size = hex_size){
 
     # Filter distance in degrees for initial filter step
-    flat <- f_centroid$latitude
-    flong <- f_centroid$longitude
+    browser()
 
+    distance <- (((f_centroid$latitude-f_centroid$latitude1)^2) + ((f_centroid$longitude-f_centroid$longitude1)^2))^(1/2)
+
+    if (distance > h_size) {
+
+    # Consider areas closer to capital city than centroid
+    angle_toward = geosphere::finalBearing(c(f_centroid$longitude, f_centroid$latitude), c(f_centroid$longitude1, f_centroid$latitude1), a = 6378160,
+        f = 1/298.257222101)
+
+    close_centroid = geosphere::destPoint(
+        p = c(f_centroid$longitude, f_centroid$latitude), b = angle_toward, d = h_size*111139,
+        a=6378160, f=1/298.257222101)
+
+    flong <- close_centroid[1]
+    flat <- close_centroid[2]
+
+    } else {
+        flong <- f_centroid$longitude
+        flat <- f_centroid$latitude
+        }
 
     grid <- f_grid %>% ungroup() %>%
         filter(flat - f_dist < hex_lat & hex_lat < flat + f_dist) %>%
         filter(flong - f_dist < hex_long & hex_long < flong + f_dist)
 
-    grid <- grid %>% mutate(
+    grid <- grid %>%
+        rowwise %>%
+        mutate(
         hex_lat_c = hex_lat - flat,
         hex_long_c = hex_long - flong) %>%
         mutate(hyp = ((hex_lat_c^2) + (hex_long_c^2))^(1/2))
