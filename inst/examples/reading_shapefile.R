bf5840dc81a0770d583eaf214fd03d175795e0f1
Steff <stephanie.kobakian@gmail.com>
2018-12-20 14:30:02 +1100

    Removed vignettes

--- inst/examples/reading_shapefile.R
+++
@@ -1,3 +0,0 @@
-# This code was used to process the raw shape files
-# Download shape file from ABS web site
-# http://www.abs.gov.au/AUSSTATS/abs@.nsf/DetailsPage/1270.0.55.001July%202016?OpenDocument
