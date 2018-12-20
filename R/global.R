eb696eb97a0b664efda8fa752da2172c83c3894e
Steff <stephanie.kobakian@gmail.com>
2018-12-20 14:34:13 +1100

    global variables definitions

---
+++ R/global.R
@@ -0,0 +1,36 @@
+# add global variables
+
+# allocate function
+utils::globalVariables(c('focal_distance',
+    'points',
+    'angle',
+    'hyp'))
+
+
+# create_buffer function
+utils::globalVariables(c('lat_int',
+'long_int',
+'long_int',
+'long_min',
+'long_max',
+'lat_int',
+'lat_min',
+'lat_max',
+'hex_long_int',
+'mean_long_min',
+'mean_long_max',
+'hex_lat_int',
+'mean_lat_min',
+'mean_lat_max',
+'lat_buffer',
+'long_buffer'))
+
+# create centroids function
+utils::globalVariables(c('X', 'Y'))
+
+#filter_grid_points function
+utils::globalVariables(c('filter_dist', 'width', 'hex_size', 'hex_lat_c', 'hex_long_c', 'latitude1', 'longitude1', 'angle', 'hyp', 'hex_angle',
+'angle_minus', 'angle_plus'))
+
+# find lat group function
+utils::globalVariables(c('next_lat', 'gradient', 'new_grad', 'cut_long'))
