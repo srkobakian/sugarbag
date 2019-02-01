67d6b115428136242bd70548f97def03c45405b6
Dianne Cook <visnut@gmail.com>
2019-02-01 16:39:40 +1100

    data load for example code

--- R/create_hexmap.R
+++ R/create_hexmap.R
@@ -25,7 +25,8 @@
 #' @examples
 #' \dontrun{
 #'
-#' hexmap <- create_hexmap(shp = sugaRbag::lga_2011, sf_id = "LGA_CODE11", buffer_dist = NULL, filter_dist = 10, hex_size = NULL, export_shp = FALSE, focal_points = sugaRbag::capital_cities, verbose = TRUE)
+#' data(lga_2011)
+#' hexmap <- create_hexmap(shp = lga_2011, sf_id = "LGA_CODE11", buffer_dist = NULL, filter_dist = 10, hex_size = NULL, export_shp = FALSE, focal_points = sugaRbag::capital_cities, verbose = TRUE)
 #' }
 #'
 create_hexmap <- function(shp = NULL, shp_path = NULL, sf_id = NULL, buffer_dist = NULL, hex_size = NULL, filter_dist = NULL, width = 15, focal_points = NULL, projstring = NULL, epsg = NULL, export_shp = FALSE, verbose = FALSE) {
