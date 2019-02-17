546b3efacc22bc762aa97a62b74ccd59d229def5
Steff <stephanie.kobakian@gmail.com>
2019-02-18 08:27:05 +1100

    Replaced Australian LGA data with Tas sa2 data

--- data-raw/tas_sa2.R
+++ data-raw/tas_sa2.R
@@ -8,7 +8,7 @@
 # www.abs.gov.au/ausstats/subscriber.nsf/log?openagent&1270055001_sa2_2011_aust_shape.zip&1270.0.55.001&Data%20Cubes&7130A5514535C5FCCA257801000D3FBD&0&July%202011&23.12.2010&Latest
 
 # unzip to a folder:
-file_path <- "~/data/SA2_2011_AUST"
+file_path <- "data/lga_2011.rda"
 
 # Load sugaRbag library
 library(sugaRbag)
@@ -17,4 +17,4 @@ library(sugaRbag)
 sa2_2011 <- read_shape(shp_path = file_path, simplify = 0.1)
 
 # to subset for Tasmania
-tas_sa2 <- filter(sa2_2011, STE_NAME11 == "Tasmania")
+tas_sa2 <- filter(sa2_2011, sa2_2011$STE_NAME11 == "Tasmania")
