1e83226d81dc78b1e1b2b4e65308dc656fbf48ce
Steff <stephanie.kobakian@gmail.com>
2019-05-01 12:42:56 +1000

    added centroid test

---
+++ tests/testthat/test-centroids.R
@@ -0,0 +1,5 @@
+test_that('centroid dimensions correct', {
+  expect_equal(expect_warning(ncol(sugarbag::create_centroids(sugarbag::tas_lga, "LGA_CODE16")), "attributes are constant"), 3)
+  
+  expect_equal(expect_warning(nrow(sugarbag::create_centroids(sugarbag::tas_lga, "LGA_CODE16")), "correct centroids for longitude"), 29)
+})
