5662febc37ee641f8c097e3f8337b524628a0796
Stephanie Kobakian <stephanie.kobakian@gmail.com>
2022-10-21 17:05:56 +1100

    2016 lga and sa2 data exmaples

--- tests/testthat/test-centroids.R
+++ tests/testthat/test-centroids.R
@@ -1,10 +1,10 @@
 test_that('centroid dimensions correct', {
   
-  expect_warning(lga_centroids <- sugarbag::create_centroids(sugarbag::tas_lga, "LGA_CODE16"))
-  expect_warning(sa2_centroids <- sugarbag::create_centroids(sugarbag::tas_sa2, "SA2_NAME16"))
+    lga_centroids <- sugarbag::create_centroids(sugarbag::tas_lga, "lga_code_2016")
+    sa2_centroids <- sugarbag::create_centroids(sugarbag::tas_sa2, "sa2_name_2016")
   
   # check projection returned correct latitude and longitudes
-  # will need to be changed when extended for nothings and eastings
+  # will need to be changed when extended for northings and eastings
   expect_success(expect_length(lga_centroids$longitude, 29))
   expect_success(expect_length(sa2_centroids$longitude, 99))
   
